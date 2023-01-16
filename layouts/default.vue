106c6368fa94f327febe9c9fc602971bb9ca3d3d
Geri0326 <79844316+Geri0326@users.noreply.github.com>
2023-01-16 16:34:33 +0100

    changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,7 @@
 <template>
-  <div>
-    <UiHeader />
+  
     <v-main>
-      <!-- <UiBreadcrumbs /> NOT WORKING AS INTENDED - DON'T USE -->
-      <slot></slot
-    ></v-main>
-  </div>
+      <slot></slot>
+    </v-main>
+
 </template>
