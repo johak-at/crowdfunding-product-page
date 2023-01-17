b661bbbd06a462bda77d85193bb4ff8e7e84faa1
Geri0326 <79844316+Geri0326@users.noreply.github.com>
2023-01-17 11:20:42 +0100

    nix weiterbrocht

---
+++ pages/cf-demo/index.vue
@@ -0,0 +1,31 @@
+<template>
+<!--
+<div class="Image">
+    <img src="@/public/image-hero-desktop.jpg" alt="Desktop">
+</div>
+-->
+
+    <h1 class="title">ASD</h1>
+
+    
+</template>
+<style>
+
+    title{
+        
+        color:black
+        /*set text color to black */
+
+        
+
+        
+    }
+
+
+    main{
+        background-color: black;
+        /*margin-left: 471.5px;
+        margin-right: 471.5px;*/
+    }
+</style>
+
