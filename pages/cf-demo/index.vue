bd1e67349e590e8e53ac6f177806a760a23f560d
Geri0326 <79844316+Geri0326@users.noreply.github.com>
2023-01-20 12:31:32 +0100

    Push

--- pages/cf-demo/index.vue
+++ pages/cf-demo/index.vue
@@ -1,9 +1,8 @@
 <template>
-<!--
 <div class="Image">
     <img src="@/public/image-hero-desktop.jpg" alt="Desktop">
 </div>
--->
+
 
     <h1 class="title">ASD</h1>
 
@@ -14,7 +13,6 @@
     title{
         
         color:black
-        /*set text color to black */
 
         
 
@@ -24,8 +22,6 @@
 
     main{
         background-color: black;
-        /*margin-left: 471.5px;
-        margin-right: 471.5px;*/
     }
 </style>
 
--- pages/index.vue
+++ pages/index.vue
@@ -31,33 +31,114 @@ definePageMeta({
 </script>
 
 <template>
-  <template>
-
-<div class="Image">
+  
+<!--<div class="Image">
     <img src="@/public/image-hero-desktop.jpg" alt="Desktop">
+</div>-->
+
+<!--Eigenes Div für die ganze Seite-->
+<div class="Seite">
+    <div class="Header">
+      <a class="Header-title">Crowdfunding</a>
+      <div class="rightside">
+      <a class="Header-Insta">Insta</a>
+      <a class="Header-about">About</a>
+      </div>
+      
+  </div>
+  <div class="cards-middle">
+    <div class="top-card-section">
+
+    </div>
+    <div class="middle-card-section"></div>
+    <div class="bottom-card-section"></div>
+
+
+  </div>
+  
 </div>
-
-
-    <h1 class="title">ASD</h1>
+    
 
     
 </template>
-</template>
 
 <style>
-title{
-        
-        background-color:black
 
-        
+.top-card-section{
+  width: 750px;
+  height: 250px;
+  border-radius: 10px;
+  /*center vertically without using margin*/
+  margin-top: 30px;
+  margin-left: 25px;
 
-        
+
+}
+.middle-card-section{
+  width: 750px;
+  height: 250px;
+  border-radius: 10px;
+  margin-top: 30px;
+  margin-left: 25px;
+
+}
+.bottom-card-section{
+  width: 750px;
+  height: 250px;
+  border-radius: 10px;
+  margin-top: 30px;
+  margin-left: 25px;
+
+}
+
+.Header{
+  display: flex;
+  justify-content: space-between;
+}
+.rightside{
+  display: flex;
+  justify-content: adjust;
+  margin-top: 3px;
+}
+
+.cards-middle{
+  width: 800px;
+  height: 100vh;
+  /**center horizontally */
+  margin: 0 auto;
+
+}
+
+.Header-about{
+  color:black;
+  font-size: 16px;
+  font-weight: bold; 
+  margin-left: 50px;
+  margin-right: 100px;
+}
+
+.Header-title{
+        color:black;
+        font-size: 20px;
+        font-weight: bold; 
+        margin: 0;
+        padding: 0;
+        margin-left: 100px;
     }
+.Header-Insta{
+        color:black;
+        font-size: 16px;
+        font-weight: 70; 
+        font-weight: bold;
 
+    }
+div{
+  border-width: 0.5px;
+ border-style:solid;
+ border-color:red
+}
 
-    main{
-        background-color: white;
-        margin-left: 471.5px;
-        margin-right: 471.5px;
+    .v-main{
+      background-color: white;
     }
 </style>
